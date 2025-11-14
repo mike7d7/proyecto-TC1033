2756008def776c11ac1b3c8361bdaa8b9d07145f
mike7d7 <mike7d7@proton.me>
2025-11-13 20:40:46 -0600

    agregar clase CuentaCorriente

---
+++ cuenta_corriente.cc
@@ -0,0 +1,5 @@
+#include "cuenta_corriente.hh"
+
+float CuentaCorriente::getLineaCredito() { return linea_credito; }
+
+void CuentaCorriente::setLineaCredito(double lc) { linea_credito = lc; }
---
+++ cuenta_corriente.hh
@@ -0,0 +1,14 @@
+#include "cuenta_bancaria.hh"
+
+class CuentaCorriente : public CuentaBancaria {
+  private:
+    double linea_credito;
+
+  public:
+    CuentaCorriente(int id, std::string ti)
+        : CuentaBancaria(id, ti, 0.0), linea_credito(5000.0) {};
+    CuentaCorriente(int id, std::string ti, double sa, double lc)
+        : CuentaBancaria(id, ti, sa), linea_credito(lc) {};
+    float getLineaCredito();
+    void setLineaCredito(double);
+};
