6e08185d4f49489466c788887de97c7c90c5c706
mike7d7 <mike7d7@proton.me>
2025-11-27 11:07:04 -0600

    main.cpp: simular borrado de pantalla con newlines

--- main.cpp
+++ main.cpp
@@ -5,43 +5,12 @@
 #include <sstream>
 #include <string>
 
-#if _WIN32
-#include <windows.h>
 void clear() {
-  HANDLE hStdOut;
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  DWORD count;
-  DWORD cellCount;
-  COORD homeCoords = {0, 0};
-
-  hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-  if (hStdOut == INVALID_HANDLE_VALUE)
-    return;
-
-  /* Get the number of cells in the current buffer */
-  if (!GetConsoleScreenBufferInfo(hStdOut, &csbi))
-    return;
-  cellCount = csbi.dwSize.X * csbi.dwSize.Y;
-
-  /* Fill the entire buffer with spaces */
-  if (!FillConsoleOutputCharacter(hStdOut, (TCHAR)' ', cellCount, homeCoords,
-                                  &count))
-    return;
-
-  /* Fill the entire buffer with the current colors and attributes */
-  if (!FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount,
-                                  homeCoords, &count))
-    return;
-
-  /* Move the cursor home */
-  SetConsoleCursorPosition(hStdOut, homeCoords);
-}
-#else
-void clear() {
-  // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
   std::cout << "\x1B[2J\x1B[H";
+  for (int i = 0; i < 100; i++) {
+    std::cout << std::endl;
+  }
 }
-#endif
 
 void menu_principal() {
   clear();
