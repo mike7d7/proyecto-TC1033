1c45083569754be62d4d8727d491ce80d2349fee
mike7d7 <mike7d7@proton.me>
2025-11-30 21:05:43 -0600

    cuentaInversion.hpp: evitar puntero basura en constructor default

--- cuentaInversion.hpp
+++ cuentaInversion.hpp
@@ -9,7 +9,7 @@ class CuentaInversion : public CuentaBancaria {
     Activo *activo;
 
   public:
-    CuentaInversion() : CuentaBancaria(0, "nadie", 0), activo() {};
+    CuentaInversion() : CuentaBancaria(0, "nadie", 0), activo(nullptr) {};
     CuentaInversion(int id, std::string ti, double sa, Activo *act)
         : CuentaBancaria(id, ti, sa), activo(act) {};
     Activo getActivo();
