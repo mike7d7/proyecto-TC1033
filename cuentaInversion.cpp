9555965fcb0e2c540a9e76f45da67302e2f999be
mike7d7 <mike7d7@proton.me>
2025-12-04 07:56:55 -0600

    cuentaInversion: agregar comentarios

--- cuentaInversion.cpp
+++ cuentaInversion.cpp
@@ -1,7 +1,12 @@
 #include "cuentaInversion.hpp"
 #include "activo.hpp"
 
+// Getter
 Activo CuentaInversion::getActivo() { return *activo; }
+
+// Se aplica el rendimiento generado como intereses a la cuenta en base a su
+// salario actual y el porcentaje de interes. El rendimiento es un atributo del
+// activo asociado a la cuenta.
 void CuentaInversion::generaRendimiento() {
   saldo += saldo * activo->getRendimiento();
 }
--- cuentaInversion.hpp
+++ cuentaInversion.hpp
@@ -1,3 +1,7 @@
+// Clase CuentaInversion, hereda de la clase CuentaBancaria y agrega el atributo
+// "activo", el cual se usa para conocer el instrumento de inversión que utiliza
+// la cuenta.
+
 #ifndef INVERSION_H
 #define INVERSION_H
 
@@ -6,13 +10,24 @@
 
 class CuentaInversion : public CuentaBancaria {
   private:
+    // Atributo de la clase
     Activo *activo;
 
   public:
+    // Constructor default, solo existe para la creación de los arreglos de
+    // cuentas en clase Banco. No se usa para crear activos nuevos.
     CuentaInversion() : CuentaBancaria(0, "nadie", 0), activo(nullptr) {};
+
+    // Constructor que recibe id, titular, saldo y activo. Los
+    // primeros tres se pasan al constructor de la clase padre. Siempre se usa
+    // este constructor para crear nuevas cuentas de inversión.
     CuentaInversion(int id, std::string ti, double sa, Activo *act)
         : CuentaBancaria(id, ti, sa), activo(act) {};
+
+    // Getter
     Activo getActivo();
+
+    // Aumenta el saldo de las cuentas que ganan intereses.
     void generaRendimiento();
 };
 
