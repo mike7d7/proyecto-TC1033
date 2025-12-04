cf893980f62ef95bb054d0db8e277b2fdb5c51ac
mike7d7 <mike7d7@proton.me>
2025-12-03 21:10:51 -0600

    activo: agregar comentarios

--- activo.cpp
+++ activo.cpp
@@ -1,8 +1,11 @@
 #include "activo.hpp"
 
+// Getters: devuelven una variable de la clase.
 int Activo::getId() { return id; };
 std::string Activo::getNombre() { return nombre; };
 float Activo::getRendimiento() { return rendimiento; };
+
+// Setters: asignan un nuevo valor a una variable de la clase.
 void Activo::setRendimiento(float new_rendimiento) {
   rendimiento = new_rendimiento;
 };
--- activo.hpp
+++ activo.hpp
@@ -1,3 +1,7 @@
+// Clase Activo, representa instrumentos de inversión.
+// Solo se usa en las cuentas de tipo inversión.
+// Tiene un id, su nombre, y el rendimiento que genera.
+
 #ifndef ACTIVO_H
 #define ACTIVO_H
 
@@ -5,14 +9,22 @@
 
 class Activo {
   private:
+    // Variables de la clase
     int id;
     std::string nombre;
     float rendimiento;
 
   public:
+    // Constructor default, solo existe para la creación de los arreglos de
+    // cuentas en clase Banco. No se usa para crear activos nuevos.
     Activo() : id(0), nombre("activo_default"), rendimiento(0.0) {};
+
+    // Constructor que recibe id, nombre y rendimiento.
+    // Siempre se usa este constructor para crear activos nuevos.
     Activo(int id, std::string nombre, float rendimiento)
         : id(id), nombre(nombre), rendimiento(rendimiento) {};
+
+    // Métodos de acceso
     int getId();
     std::string getNombre();
     float getRendimiento();
