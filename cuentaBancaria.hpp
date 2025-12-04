d72946935afacffc0084e1554c4fc7e24bc52409
mike7d7 <mike7d7@proton.me>
2025-12-03 21:24:25 -0600

    cuentaBancaria.hpp: quitar constructor innecesario

--- cuentaBancaria.hpp
+++ cuentaBancaria.hpp
@@ -1,3 +1,7 @@
+// Clase CuentaBancaria, clase padre de todas las clases de Cuentas
+// especializadas. Tiene un id único, un titular (persona dueña de la cuenta) y
+// un saldo. Le permite a todas las cuentas hacer retiros y recibir depósitos.
+
 #ifndef CUENTA_BANCARIA_H
 #define CUENTA_BANCARIA_H
 
@@ -5,13 +9,12 @@
 
 class CuentaBancaria {
   protected:
+    // Atributos de la clase, los cuales también usan las clases hijas.
     int id;
     std::string titular;
     double saldo;
 
   public:
-    CuentaBancaria(int id, std::string titular)
-        : id(id), titular(titular), saldo(0.0) {};
     CuentaBancaria(int id, std::string titular, double saldo)
         : id(id), titular(titular), saldo(saldo) {};
     int getId();
