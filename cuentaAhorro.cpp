8ba08426bfd73e3c0229282468730254ee1cc4ed
mike7d7 <mike7d7@proton.me>
2025-12-03 21:16:00 -0600

    cuentaAhorro: agregar comentarios

--- cuentaAhorro.cpp
+++ cuentaAhorro.cpp
@@ -1,7 +1,11 @@
 #include "cuentaAhorro.hpp"
 
+// Getter
 float CuentaAhorro::getInteres() { return interes; }
 
+// Setter
 void CuentaAhorro::setInteres(float in) { interes = in; }
 
+// Se aplica el rendimiento generado como intereses a la cuenta en base a su
+// salario actual y el porcentaje de interes.
 void CuentaAhorro::generaInteres() { saldo += saldo * interes; }
--- cuentaAhorro.hpp
+++ cuentaAhorro.hpp
@@ -1,3 +1,7 @@
+// Clase CuentaAhorro, hereda de la clase CuentaBancaria y agrega el atributo
+// "interes", el cual se usa para que se genere un rendimiento en base al saldo
+// de la cuenta.
+
 #ifndef AHORRO_H
 #define AHORRO_H
 
@@ -5,14 +9,28 @@
 
 class CuentaAhorro : public CuentaBancaria {
   private:
+    // Atributo de la clase.
     float interes;
 
   public:
+    // Constructores
+    // Constructor default, solo existe para la creación de los arreglos de
+    // cuentas en clase Banco. No se usa para crear activos nuevos.
     CuentaAhorro() : CuentaBancaria(0, "nadie", 0.0), interes(0) {};
+
+    // Constructor que recibe id, titular, saldo e interés. Los primeros tres se
+    // pasan al constructor de la clase padre. Siempre se usa este constructor
+    // para crear nuevas cuentas de ahorro.
     CuentaAhorro(int id, std::string titular, double sa, float in)
         : CuentaBancaria(id, titular, sa), interes(in) {};
+
+    // Getter
     float getInteres();
+
+    // Setter
     void setInteres(float);
+
+    // Aumenta el saldo de las cuentas que ganan intereses.
     void generaInteres();
 };
 
