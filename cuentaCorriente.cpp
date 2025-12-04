188a9e927799cbf9e7b5a498ec2a5e718a189782
mike7d7 <mike7d7@proton.me>
2025-12-04 07:54:04 -0600

    cuentaCorriente: agregar comentarios

--- cuentaCorriente.cpp
+++ cuentaCorriente.cpp
@@ -1,9 +1,14 @@
 #include "cuentaCorriente.hpp"
 
+// Getter
 double CuentaCorriente::getLineaCredito() { return linea_credito; }
 
+// Setter
 void CuentaCorriente::setLineaCredito(double lc) { linea_credito = lc; }
 
+// Funciona como el método "retirar" de la clase cuentaBancaria, pero en lugar
+// de checar solo el saldo, checa si completa la cantidad a retirar sumando el
+// saldo y la línea de crédito.
 int CuentaCorriente::retirar(double cantidad) {
   if (cantidad <= saldo + linea_credito) {
     linea_credito -= cantidad;
--- cuentaCorriente.hpp
+++ cuentaCorriente.hpp
@@ -1,3 +1,6 @@
+// Clase CuentaCorriente, hereda de la clase CuentaBancaria y agrega el atributo
+// "linea_credito", el cual se usa como si fuera saldo adicional.
+
 #ifndef CORRIENTE_H
 #define CORRIENTE_H
 
@@ -5,15 +8,28 @@
 
 class CuentaCorriente : public CuentaBancaria {
   private:
+    // Atributo de la clase
     double linea_credito;
 
   public:
+    // Constructor default, solo existe para la creación de los arreglos de
+    // cuentas en clase Banco. No se usa para crear activos nuevos.
     CuentaCorriente()
         : CuentaBancaria(0, "nadie", 0.0), linea_credito(5000.0) {};
+
+    // Constructor que recibe id, titular, saldo y línea de crédito. Los
+    // primeros tres se pasan al constructor de la clase padre. Siempre se usa
+    // este constructor para crear nuevas cuentas corrientes.
     CuentaCorriente(int id, std::string ti, double sa, double lc)
         : CuentaBancaria(id, ti, sa), linea_credito(lc) {};
+
+    // Getter
     double getLineaCredito();
+
+    // Setter
     void setLineaCredito(double);
+
+    // Sobreescribir método retirar (proviene de la clase padre).
     int retirar(double);
 };
 
